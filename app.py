5f3534dd871b671b33e8d80d3bad587e61f15253
jam <27138217@qq.com>
2022-10-15 19:46:43 +0800

    the bootstrap part

--- app.py
+++ app.py
@@ -1,42 +1,61 @@
 from colorama import Cursor
-from flask import Flask
+from flask import Flask,render_template,url_for
 from datetime import datetime
 import json,time
-import mysql.connector
+#import mysql.connector
 
 
 app = Flask(__name__)
+posts = [
+    {
+        'author': 'jam',
+        'title': 'blog post1',
+        'content': 'First post content',
+        'date_posted': 'October 1,2022'
+    },
+    {
+        'author': 'hether',
+        'title': 'Blog post2',
+        'content': 'Second post content',
+        'date_posted': 'october 2,2022'
+    }
+]
+
 
 @app.route("/")
-def hello_world():
-    t = datetime.now().isoformat(timespec='minutes')
-    return "<h1>Hello docker! @2022/5/31(23:56)</h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts,title='20221013')
 
-@app.route("/widgets")
-def get_widgets():
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="xiaoniu@jam",
-        database="jam_flask_db"
-    )
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
-    cursor = mydb.cursor()
+# @app.route("/widgets")
+# def get_widgets():
+#     mydb = mysql.connector.connect(
+#         host="mysqldb",
+#         user="root",
+#         password="xiaoniu@jam",
+#         database="jam_flask_db"
+#     )
 
-    cursor.execute("select * from widgets")
+#     cursor = mydb.cursor()
 
-    row_headers=[x[0] for x in cursor.description] #this will extract row headers
+#     cursor.execute("select * from widgets")
 
-    results = cursor.fetchall()
-    json_data=[]
-    for result in results:
-        json_data.append(dict(zip(row_headers,result)))
+#     row_headers=[x[0] for x in cursor.description] #this will extract row headers
 
-    cursor.close()
+#     results = cursor.fetchall()
+#     json_data=[]
+#     for result in results:
+#         json_data.append(dict(zip(row_headers,result)))
+
+#     cursor.close()
 
 
-    return json.dumps(json_data)
-    #return "test"
+#     return json.dumps(json_data)
+#     #return "test"
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0")
